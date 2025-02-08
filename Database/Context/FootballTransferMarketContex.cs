6d408a4cfd194d8a8019ac999155568deb8e1941
HP <sahadathossain82865@gmail.com>
2025-02-08 12:08:22 +0600

    class 14

---
+++ Database/Context/FootballTransferMarketContex.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using Database.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database.Context
+{
+    public class FootballTransferMarketContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(@"Server=DESKTOP-POOH2AD\SQLEXPRESS;Database=FootballTransferMarket;Trusted_Connection=True;TrustServerCertificate=True;ConnectRetryCount=0");
+            optionsBuilder.UseSqlServer(@"Server=.;Database=FootballTransferMarket;Trusted_Connection=True;TrustServerCertificate=True;ConnectRetryCount=0");
+        }
+       
+    }
+}
---
+++ Database/DBTest/Class1.cs
@@ -0,0 +1,19 @@
+﻿using Database.Context;
+using Database.Model;
+using System.Data;
+
+namespace Database.DBTest
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            FootballTransferMarket db = new FootballTransferMarket();
+
+            var AllUser = db.UserRoleInfo.ToList();
+
+
+
+        }
+    }
+}
